1dcace71b93c0759db980ff114c5f451a6f9e818
Adrian Pfaff <mail@adrian-pfaff.de>
2021-05-27 18:15:36 +0200

    added Object Base Class

---
+++ src/Core/Object.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <memory>
+#include "ObjectTypes.h"
+class Object : public std::enable_shared_from_this<Object> {
+public:
+    explicit Object(ObjectTypes type);
+    virtual void Update()=0;
+    virtual void Draw()=0;
+    [[nodiscard]] ObjectTypes GetType() const;
+    virtual ~Object()=default;
+
+protected:
+    ObjectTypes type;
+
+private:
+
+};
+
+
---
+++ src/Core/ObjectTypes.h
@@ -0,0 +1,3 @@
+#pragma once
+
+enum ObjectTypes{Player=0, Enemy=1, Interactable=2};
