d9f0709d8fcd1c4c0eaffd20ac4b50f0aef5a458
Adrian Pfaff <mail@adrian-pfaff.de>
2021-05-27 19:16:14 +0200

    added Interactable class, minor whitespace correction

--- src/Core/Object.h
+++ src/Core/Object.h
@@ -5,10 +5,10 @@
 class Object : public std::enable_shared_from_this<Object> {
 public:
     explicit Object(ObjectTypes type);
-    virtual void Update()=0;
-    virtual void Draw()=0;
+    virtual void Update() = 0;
+    virtual void Draw() = 0;
     [[nodiscard]] ObjectTypes GetType() const;
-    virtual ~Object()=default;
+    virtual ~Object() = default;
 
 protected:
     ObjectTypes type;
