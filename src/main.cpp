27edbd79e21d0fa106c2835040f497dd6ac3cc6d
Adrian Pfaff <mail@adrian-pfaff.de>
2021-05-28 19:48:47 +0200

    readded unique_ptr in main, still not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <cstdlib>
+#include <memory>
 
 #include "raylib.h"
 
@@ -11,7 +12,7 @@
 
 int main() {
 
-    SceneManager sceneManager(std::make_unique<MainMenu>(), std::make_unique<IgniteGameMode>());
+    std::unique_ptr<SceneManager> sceneManager=std::make_unique<SceneManager>(std::make_unique<MainMenu>(), std::make_unique<IgniteGameMode>());
     // Enable config flags for resizable window and vertical synchro
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
     InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
