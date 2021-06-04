b412f3b7b00a319f38ed72356e2927f5be74e679
Adrian Pfaff <mail@adrian-pfaff.de>
2021-06-04 11:55:42 +0200

    made scene manager global

--- src/Global.h
+++ src/Global.h
@@ -1,5 +1,7 @@
 #pragma once
+class SceneManager;
 
 extern std::shared_ptr<PlayerCharacter> playerCharacter;
 extern std::shared_ptr<PlayerController> playerController;
 extern std::shared_ptr<HUD> hud;
+extern std::shared_ptr<SceneManager> sceneManager;
