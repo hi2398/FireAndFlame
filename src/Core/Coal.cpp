0979f52303f9a0c82a3477ceefbb2b57bef73f50
Robin Günther <11014686@STUD.HOCHSCHULE-HEIDELBERG.DE>
2021-06-06 17:07:47 +0200

    Kohle! Jetzt auch zum mitnehmen!

--- src/Core/Coal.cpp
+++ src/Core/Coal.cpp
@@ -2,36 +2,80 @@
 
 Coal::Coal()
 {
+	aTexture = LoadTexture("assets/graphics/Coal.png");
 }
 
 void Coal::Interact()
 {
-	playerCharacter->SetHealth(playerCharacter->GetHealth() + Fuel);
+	if (aEnabled == true)
+	{
+		playerCharacter->SetHealth(playerCharacter->GetHealth() + aFuel);
+		aEnabled = false;
+		aFallSpeed = 1.5*aGravityMultiplyer;
+	}
 }
 
-void Coal::SetPosition(Vector2 Pos)
+void Coal::SetPosition(Vector2 pPos)
 {
-	aPos = Pos;
+	aPos = pPos;
 }
 
-void Coal::SetTexture(Texture2D Texture)
+void Coal::SetTexture(Texture2D pTexture)
 {
-	aTexture = Texture;
+	aTexture = pTexture;
+}
+
+void Coal::SetGrounded(bool pIsGroudned)
+{
+	aisGounded = pIsGroudned;
+}
+
+bool Coal::GetGrounded()
+{
+	return aisGounded;
+}
+
+bool Coal::GetEnabled()
+{
+	return aEnabled;
+}
+
+void Coal::SetEnabled(bool pEnabled)
+{
+	aEnabled = pEnabled;
+}
+
+
+
+
+Rectangle Coal::GetHitbox()
+{
+	return aHitbox;
 }
 
 void Coal::Update()
 {
-	if (!isGounded) 
+	if (!aisGounded)
 	{	//Wenn es nicht Am Boden ist, fällt es runter
-		aPos.y += 0.1f;
+		aPos.y += aFallSpeed;
+		aFallSpeed += 0.1f;
 	}
+	aHitbox.width = aTexture.width;
+	aHitbox.height = aTexture.height;
+	aHitbox.x = aPos.x;
+	aHitbox.y = aPos.y;
+
 }
 
 void Coal::Draw()
 {
-	DrawTexture(aTexture, aPos.x, aPos.y, WHITE);
+	if (aEnabled == true)
+	{
+		DrawTexture(aTexture, aPos.x, aPos.y, WHITE);
+	}
 }
 
 Coal::~Coal()
 {
+	UnloadTexture(aTexture);
 }
--- src/Core/Coal.h
+++ src/Core/Coal.h
@@ -2,23 +2,39 @@
 #include "Interactable.h"
 #include "raylib.h"
 #include "../Global.h"
+#include <iostream>
+
+/*This Class was made by Robin. Therefore following prefixes apply:
+For an Attribute the prefix "a" applies
+For a Parameter the prefix "p" applies
+*/
+
+
 
 class Coal:public Interactable
 {
 public:
 	Coal();
 	void Interact()override;
-	void SetPosition(Vector2 Pos);
-	void SetTexture(Texture2D Texture);
+	void SetPosition(Vector2 pPos);
+	void SetTexture(Texture2D pTexture);
+	void SetGrounded(bool pIsGroudned);
+	bool GetGrounded();
+	bool GetEnabled();
+	void SetEnabled(bool pEnabled);
+	Rectangle GetHitbox();
 	void Update();
 	void Draw();
 	~Coal();
 
 private:
-	int Fuel = 10;
-	Texture2D aTexture;
-	Rectangle aHitbox;
-	Vector2 aPos;
-	bool isGounded;
+	int aFuel = 10;
+	Texture2D aTexture{};
+	Vector2 aPos{};
+	Rectangle aHitbox{};
+	bool aisGounded = false;
+	bool aEnabled = false;
+	int aFallSpeed = 1.5;
+	int aGravityMultiplyer = 1.5;
 };
 
