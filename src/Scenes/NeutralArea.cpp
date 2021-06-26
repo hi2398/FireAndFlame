73e654ed063fd9edee70f03671c350e6b5759e8f
Pascal Hirt <pascal.hirt@stud.hochschule-heidelberg.de>
2021-06-26 17:29:43 +0200

    tilemap

--- src/Scenes/NeutralArea.cpp
+++ src/Scenes/NeutralArea.cpp
@@ -5,7 +5,7 @@
 
 
 NeutralArea::NeutralArea() : Scene("assets/Dialogues/testText.json") {
-    tilemap=std::make_unique<Tilemap>("assets/Tilemaps/Testmap/Placehalter_2.json","assets/Tilemaps/Testmap/Map_2.json");
+    tilemap=std::make_unique<Tilemap>("assets/Tilemaps/Testmap/Placehalter_2.json","assets/Tilemaps/Testmap/NEUTRAL_AREA.json");
 }
 
 void NeutralArea::Update() {
