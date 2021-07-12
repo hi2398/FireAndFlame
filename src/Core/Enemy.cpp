6aeb4755eb29c5b447dac5efcd309a920f474d89
Robin Günther <11014686@STUD.HOCHSCHULE-HEIDELBERG.DE>
2021-07-12 16:50:13 +0200

    Fertiger Gegner Miner

--- src/Core/Enemy.cpp
+++ src/Core/Enemy.cpp
@@ -25,30 +25,22 @@ bool Enemy::CheckLineOfSight(Vector2 startLocation, Vector2 endLocation, const s
 	pUnitvector.x = pLineOfSightVector.x / distance;
 	pUnitvector.y = pLineOfSightVector.y / distance;
 	Vector2 i = startLocation;
-	std::cout << "Unitvector.x: " << pUnitvector.x << "\n";
-	std::cout << "Unitvector.y: " << pUnitvector.y << "\n";
-	//std::cout << "Distance: " << distance << "\n";
-	//std::cout << "Line of sight initialised\n";
-	while (CheckCollisionPointRec(i, playerCharacter->visibleScreen))
+	if (CheckCollisionPointRec(i, playerCharacter->visibleScreen))
 	{
 		i.x =i.x+ pUnitvector.x*20;
 		i.y =i.y+ pUnitvector.y*20;
-		//.x += 10;
-		//std::cout << "following unitvector\n";
 		Rectangle tileRec = { 0,0,32,32 };
 		for (const auto& collTile : tilemap->GetTileColliders()) {
 			tileRec.x = collTile.x;
 			tileRec.y = collTile.y;
-			//std::cout << i.x<< "x und y="<<i.y<<"\n";
-			//std::cout << "Endlocation x: " << endLocation.x << ", y: " << endLocation.y << "\n";
 			if (CheckCollisionPointRec(i, playerCharacter->playerHitbox)) 
 			{
-				//std::cout << "ich sehe dich\n";
+				std::cout << "ich sehe dich\n";
 				return true;
 			}
-			else if (CheckCollisionPointRec(i, tileRec))
+			else if (CheckCollisionPointRec(i, tileRec)||!CheckCollisionPointRec(i, playerCharacter->visibleScreen))
 			{   
-				//std::cout << "Ja wo isser denn?\n";
+				std::cout << "Ja wo isser denn?\n";
                 return false;
 			}
 			else
@@ -62,9 +54,7 @@ bool Enemy::CheckLineOfSight(Vector2 startLocation, Vector2 endLocation, const s
 float Enemy::GetDistance(Vector2 startLocation, Vector2 endLocation)
 {
 	float x = endLocation.x - startLocation.x;	//Differenz auf der X Achse ermitteln
-	if (x < 0) x = x * -1;				//Sichergehen, dass die Differenz positiv ist
 	float y = endLocation.y - startLocation.y;	//Differenz auf der Y Achse ermitteln
-	if (y <= 0) y = y * -1;				//Sichergehen dass die Differenz positiv ist
 	float pDistance = sqrt(pow(x, 2) + pow(y, 2));
 	return pDistance;
 }
