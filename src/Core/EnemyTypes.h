a745f8680acb24880f9a452225a571415ee220ad
Adrian Pfaff <mail@adrian-pfaff.de>
2021-07-14 11:10:36 +0200

    added empty boss class for ice boss, updated cmake lists

--- src/Core/EnemyTypes.h
+++ src/Core/EnemyTypes.h
@@ -1,3 +1,3 @@
 #pragma once
 
-enum class EnemyTypes{ToastCat=0, Miner=1, WatchBot=2, Flyer=3, SpringHog=4, Howler=5, Saugi=6 };
+enum class EnemyTypes{ToastCat=0, Miner=1, WatchBot=2, Flyer=3, SpringHog=4, Howler=5, Saugi=6, Boss };
---
+++ src/Data/IceBoss/IceBoss.cpp
@@ -0,0 +1,15 @@
+#include "IceBoss.h"
+
+IceBoss::IceBoss() : Enemy(EnemyTypes::Boss) {
+
+}
+
+
+void IceBoss::Draw() {
+
+}
+
+void IceBoss::Update() {
+
+}
+
