148807a0d846e07da93285044c6fcc9adfa068a0
Amar Civic <civic.amar@yahoo.de>
2021-07-18 08:59:06 +0200

    added NPCs in Tutorial

--- src/Scenes/Tutorial.cpp
+++ src/Scenes/Tutorial.cpp
@@ -9,10 +9,15 @@ Tutorial::Tutorial() {
     tilemap=std::make_unique<Tilemap>("assets/Tilemaps/Testmap/Placehalter_2.json","assets/Tilemaps/Testmap/Tutorial.json");
     interactables.emplace_back(std::make_unique<DialogueObject>("assets/Dialogues/tutorialText1.json",npc1Pos,npc1Tex));
     interactables.emplace_back(std::make_unique<DialogueObject>("assets/Dialogues/tutorialText2.json",npc2Pos,npc1Tex));
+    interactables.emplace_back(std::make_unique<DialogueObject>("assets/Dialogues/tutorialText3.json",npc3Pos,npc1Tex));
+    interactables.emplace_back(std::make_unique<DialogueObject>("assets/Dialogues/tutorialText4.json",npc4Pos,npc1Tex));
+    interactables.emplace_back(std::make_unique<DialogueObject>("assets/Dialogues/tutorialText5.json",npc5Pos,npc1Tex));
 }
 
 void Tutorial::Update() {
-
+    if(preventHealthDecrease){
+        playerCharacter->SetPlayerDecreasingHealth(false);
+    }
 }
 
 void Tutorial::Draw() {
--- src/Scenes/Tutorial.h
+++ src/Scenes/Tutorial.h
@@ -12,9 +12,15 @@ public:
     ~Tutorial() override = default;
 
 private:
+    bool preventHealthDecrease = true;
+
     Texture2D npc1Tex;
+    Texture2D npc2Tex;
     Vector2 npc1Pos = {35*32,99*32};
     Vector2 npc2Pos = {81*32,76*32};
+    Vector2 npc3Pos = {65*32,76*32};
+    Vector2 npc4Pos = {56*32,76*32};
+    Vector2 npc5Pos = {47*32,76*32};
     static constexpr Vector2 playerStart{ 32 * 32, 99 * 32 };
 
 protected:
