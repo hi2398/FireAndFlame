6a4c1b4734b3d6563256180c4d73c6773b896713
Amar Civic <civic.amar@yahoo.de>
2021-07-18 10:18:26 +0200

    added Tutorial mechanics

--- src/Scenes/Tutorial.h
+++ src/Scenes/Tutorial.h
@@ -23,5 +23,25 @@ private:
     Vector2 npc5Pos = {47*32,76*32};
     static constexpr Vector2 playerStart{ 32 * 32, 99 * 32 };
 
+    static constexpr Rectangle healthDecreasingCollider = {40*32,45*32,128,128}; // If player collides he will start losing health
+
+    Vector2 door1[2];
+    bool door1Active = true;
+    bool tutorial1 = false;
+    int attackCounter = 0;
+    int attackTicker = 123;
+    Rectangle tutorial1Trigger = {67*32,75*32,32,64};
+
+    Vector2 door2[2];
+    bool door2Active = true;
+    bool tutorial2 = false;
+    int heavyAttackTicker = 0;
+    Rectangle tutorial2Trigger = {58*32,75*32,32,64};
+
+    Vector2 door3[2];
+    bool door3Active = true;
+    bool tutorial3 = false;
+    Rectangle tutorial3Trigger = {49*32,75*32,32,64};
+
 protected:
 };
