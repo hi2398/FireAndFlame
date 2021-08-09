4584f06337493ec7404c3de02ec393b48e298afc
Amar Civic <civic.amar@yahoo.de>
2021-08-09 20:18:51 +0200

    Added SceneChangerObj to AreaTwo and AreOne to Bossfight

--- src/Scenes/AreaTwo.cpp
+++ src/Scenes/AreaTwo.cpp
@@ -4,6 +4,8 @@
 AreaTwo::AreaTwo(){
     playerCharacter->SetPosition(playerStart);
     tilemap=std::make_unique<Tilemap>("assets/Tilemaps/Testmap/Tilemap_1.json","assets/Tilemaps/Area_Two_Tilemap.json");
+    Vector2 tempVec= {82*32,23*32};
+    interactables.emplace_back(std::make_unique<SceneChangerObject>(tempVec,SceneEnums::TraitorBoss));
 }
 
 void AreaTwo::Update() {
