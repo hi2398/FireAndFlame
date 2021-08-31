70fae50ff0f7041d5c8d60f289399b956eaa1dc0
Amar Civic <civic.amar@yahoo.de>
2021-08-31 18:54:19 +0200

    Added Enemies to Areas

--- src/Scenes/AreaOne.cpp
+++ src/Scenes/AreaOne.cpp
@@ -3,6 +3,12 @@
 //
 #include "../Global.h"
 #include "AreaOne.h"
+#include "../Data/Enemies/Fly.h"
+#include "../Data/Enemies/Miner.h"
+#include "../Data/Enemies/Saugi.h"
+#include "../Data/Enemies/SpiderBot.h"
+#include "../Data/Enemies/SpringHog.h"
+#include "../Data/Enemies/ToastCat.h"
 
 AreaOne::AreaOne(SceneEnums lastScene) : Scene(SceneEnums::AreaOne) {
     this->lastScene = lastScene;
@@ -10,6 +16,61 @@ AreaOne::AreaOne(SceneEnums lastScene) : Scene(SceneEnums::AreaOne) {
     tilemap=std::make_unique<Tilemap>("assets/Tilemaps/Testmap/Tilemap_1.json","assets/Tilemaps/Area_One_Tilemap.json");
     Vector2 tempVec= {47*25,29*26};
     interactables.emplace_back(std::make_unique<SceneChangerObject>(tempVec,SceneEnums::MinerBoss, sceneName));
+
+    tempVec = {64*32,103*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Low));
+    tempVec = {76*32,100*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Low));
+    tempVec = {50*32,67*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Low));
+    tempVec = {57*32,42*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Low));
+    tempVec = {80*32,84*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Low));
+    tempVec = {83*32,84*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Low));
+    tempVec = {48*32,69*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Low));
+    tempVec = {53*32,69*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Low));
+    tempVec = {59*32,69*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Low));
+    tempVec = {87*32,26*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Low));
+    tempVec = {66*32,89*32};
+    enemies.emplace_back(std::make_unique<Saugi>(tempVec));
+    tempVec = {43*32,80*32};
+    enemies.emplace_back(std::make_unique<Saugi>(tempVec));
+    tempVec = {52*32,69*32};
+    enemies.emplace_back(std::make_unique<Saugi>(tempVec));
+    tempVec = {57*32,107*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Low));
+    tempVec = {75*32,67*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Low));
+    tempVec = {69*32,50*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Low));
+    tempVec = {54*32,44*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Low));
+    tempVec = {66*32,54*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Low));
+    tempVec = {77*32,38*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Low));
+    tempVec = {75*32,84*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Low));
+    tempVec = {63*32,84*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Low));
+    tempVec = {62*32,48*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Low));
+    tempVec = {77*32,26*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Low));
+    tempVec = {69*32,26*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Low));
+    tempVec = {73*32,84*32};
+    enemies.emplace_back(std::make_unique<ToastCat>(tempVec));
+    tempVec = {61*32,84*32};
+    enemies.emplace_back(std::make_unique<ToastCat>(tempVec));
+    tempVec = {74*32,38*32};
+    enemies.emplace_back(std::make_unique<ToastCat>(tempVec));
 
 
     //background initialization
--- src/Scenes/AreaTwo.cpp
+++ src/Scenes/AreaTwo.cpp
@@ -1,5 +1,12 @@
 
 #include "AreaTwo.h"
+#include "../Data/Enemies/Fly.h"
+#include "../Data/Enemies/Miner.h"
+#include "../Data/Enemies/Saugi.h"
+#include "../Data/Enemies/SpiderBot.h"
+#include "../Data/Enemies/SpringHog.h"
+#include "../Data/Enemies/ToastCat.h"
+#include "../Data/Enemies/Howler.h"
 
 AreaTwo::AreaTwo(SceneEnums lastScene) : Scene(SceneEnums::AreaTwo) {
     this->lastScene = lastScene;
@@ -7,6 +14,45 @@ AreaTwo::AreaTwo(SceneEnums lastScene) : Scene(SceneEnums::AreaTwo) {
     tilemap=std::make_unique<Tilemap>("assets/Tilemaps/Testmap/Tilemap_1.json","assets/Tilemaps/Area_Two_Tilemap.json");
     Vector2 tempVec= {96*32,46*32};
     interactables.emplace_back(std::make_unique<SceneChangerObject>(tempVec,SceneEnums::TraitorBoss, sceneName));
+
+    tempVec = {66*32,102*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Medium));
+    tempVec = {57*32,102*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Medium));
+    tempVec = {52*32,71*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Medium));
+    tempVec = {66*32,72*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Medium));
+    tempVec = {71*32,57*32};
+    enemies.emplace_back(std::make_unique<Fly>(tempVec,EnemyLevel::Medium));
+    tempVec = {40*32,99*32};
+    enemies.emplace_back(std::make_unique<Howler>(tempVec,EnemyLevel::Low));
+    tempVec = {96*32,88*32};
+    enemies.emplace_back(std::make_unique<Howler>(tempVec,EnemyLevel::Low));
+    tempVec = {88*32,76*32};
+    enemies.emplace_back(std::make_unique<Howler>(tempVec,EnemyLevel::Low));
+    tempVec = {77*32,110*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Medium));
+    tempVec = {73*32,103*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Medium));
+    tempVec = {85*32,76*32};
+    enemies.emplace_back(std::make_unique<Miner>(tempVec,EnemyLevel::Medium));
+    tempVec = {71*32,103*32};
+    enemies.emplace_back(std::make_unique<Saugi>(tempVec));
+    tempVec = {39*32,99*32};
+    enemies.emplace_back(std::make_unique<Saugi>(tempVec));
+    tempVec = {62*32,103*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Medium));
+    tempVec = {69*32,90*32};
+    enemies.emplace_back(std::make_unique<SpiderBot>(tempVec,EnemyLevel::Medium));
+    tempVec = {92*32,112*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Medium));
+    tempVec = {51*32,90*32};
+    enemies.emplace_back(std::make_unique<SpringHog>(tempVec,EnemyLevel::Medium));
+    tempVec = {101*32,112*32};
+    enemies.emplace_back(std::make_unique<ToastCat>(tempVec));
+    tempVec = {51*32,90*32};
+    enemies.emplace_back(std::make_unique<ToastCat>(tempVec));
 
     //background initialization
     textureForegroundException = LoadTexture("assets/graphics/backgrounds/AreaTwo/Lower_Foreground.png");
