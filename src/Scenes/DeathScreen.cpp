a0a6285633b7f0fdc99b7950e8eb95e71cb519a9
Amar Civic <civic.amar@yahoo.de>
2021-09-01 13:42:29 +0200

    Reworked Deathscreen

--- src/Scenes/DeathScreen.cpp
+++ src/Scenes/DeathScreen.cpp
@@ -6,16 +6,18 @@
 DeathScreen::DeathScreen(SceneEnums lastScene) : Scene(SceneEnums::Default) {
     this->lastScene = lastScene;
     tilemap=std::make_unique<Tilemap>();
+    background = LoadTexture("assets/graphics/GUI/deathScreen.png");
     playerCharacter->active = false; // Disables Player
 }
 
 void DeathScreen::Update() {
-    if(IsKeyPressed(KEY_F) || IsGamepadButtonDown(0,GAMEPAD_BUTTON_RIGHT_FACE_DOWN)){
-        sceneManager->SetNextScene(std::make_unique<NeutralArea>(sceneName));
+    if(IsKeyPressed(KEY_E) || IsGamepadButtonDown(0,GAMEPAD_BUTTON_RIGHT_FACE_DOWN)){
+        sceneManager->SetNextScene(std::make_unique<NeutralArea>(sceneName)); // TODO Load Save :Adrian:
     }
 }
 
-void DeathScreen::Draw() { // TODO Rework Death Screen looks
-    DrawText("YU DED",570,200,40,WHITE);
-    DrawText("Press F or Gamepad A to continue", 440,500,25,WHITE);
+void DeathScreen::Draw() {
+    DrawTexture(background,0,0,WHITE);
+    DrawText("You Died",485,100,60,WHITE);
+    DrawText("Press E or Gamepad A to continue", 410,250,25,WHITE);
 }
