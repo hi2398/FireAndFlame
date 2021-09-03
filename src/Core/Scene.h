dde5b5a52bf1eaad67c595cc05c1d062359c42a4
Adrian Pfaff <mail@adrian-pfaff.de>
2021-09-03 11:29:15 +0200

    added nodiscard

--- src/Core/Scene.h
+++ src/Core/Scene.h
@@ -34,7 +34,7 @@ public:
 
     void ActivateScreenShake(int durationInSeconds);
 
-    SceneEnums GetSceneName() const;
+    [[nodiscard]] SceneEnums GetSceneName() const;
 
 protected:
     SceneEnums sceneName{SceneEnums::Default};
