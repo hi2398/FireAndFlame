1b0bdbdb7684e5d2762bb464f2e2639e6956388b
Pascal Hirt <pascal.hirt@stud.hochschule-heidelberg.de>
2021-09-04 22:29:56 +0200

    deathscreen fadeout and sound

--- src/Scenes/DeathScreen.cpp
+++ src/Scenes/DeathScreen.cpp
@@ -14,10 +14,14 @@ void DeathScreen::Update() {
     if(IsKeyPressed(KEY_E) || IsGamepadButtonDown(0,GAMEPAD_BUTTON_RIGHT_FACE_DOWN)){
         sceneManager->SetNextScene(std::make_unique<NeutralArea>(sceneName)); // TODO Load Save :Adrian:
     }
+    if (FADEOUT.a > 0)FADEOUT.a -= 3;
+    if (FADEOUT.a < 0) FADEOUT.a = 0;
+    if (FADEOUT.a == 105) soundManager->PlaySfx(SFX::DEATHSCREEN);
 }
 
 void DeathScreen::Draw() {
     DrawTexture(background,0,0,WHITE);
     DrawText("You Died",485,100,60,WHITE);
     DrawText("Press E or Gamepad A to continue", 410,250,25,WHITE);
+    DrawRectangle(-100,-100, 10000, 10000, FADEOUT);
 }
