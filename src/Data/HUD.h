bfd35fa75cfbae17cefa33d422fa93d5b1ada622
Adrian Pfaff <mail@adrian-pfaff.de>
2021-09-07 10:18:43 +0200

    added save text

--- src/Data/HUD.h
+++ src/Data/HUD.h
@@ -9,6 +9,8 @@ public:
     void changeInteractable(bool interactable);
     void executeEndscreenSwap();
     void SetBossEnemyHealth(int bossHealth);
+    void ShowSaveNotification();
+
     ~HUD() = default;
 protected:
 
@@ -21,6 +23,11 @@ private:
     Color endscreenColor = {0,0,0,255};
     int bossHealth = 0;
     Rectangle bossHealthBar{};
+
+    //Save Notification
+    bool showNotification{false};
+    const int notificationTimer{60*3}; //show for 3 secs
+    int currentNotificationTimer{0};
 };
 
 
